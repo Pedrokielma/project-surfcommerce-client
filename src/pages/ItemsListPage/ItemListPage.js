4b27539fb92c191ebb1d1a319279b34f88d8b89f
PedroKielma <peterkielma@gmail.com>
2022-03-11 18:21:08 +0000

    config of packeges, first steps of components and pages

---
+++ src/pages/ItemsListPage/ItemListPage.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { ThemeContext } from '../../context/theme.context';
+
+function ItemsListPage() {
+  const [projects, setProjects] = useState([]);
+
+  const { theme } = useContext(ThemeContext);
+
+  const fetchProjects = async () => {
+    try {
+      const storedToken = localStorage.getItem('authToken');
+
+      let response = await axios.get(`${process.env.REACT_APP_API_URL}/projects`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      });
+      setProjects(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  return (
+    <div className={'ProjectsPage ' + theme}>
+      <AddForm refreshProjects={fetchProjects} />
+      {projects.map((project) => {
+        return (
+          <div key={project._id}>
+            <Link to={`/projects/${project._id}`}>
+              <h3>{project.title}</h3>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ItemstsListPage;
